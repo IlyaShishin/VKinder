a0828597483b8bd5b680c2caa773eea1b7979d2d
Ilya Shishin <Shishinilya030996@mail.ru>
2021-07-21 19:35:14 +0400

    First commit
    First commit

---
+++ package_data/connect_database.py
@@ -0,0 +1,20 @@
+import sqlalchemy as sq
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+DSN = ''
+engine = sq.create_engine(DSN)
+# connect = engine.connect()
+session = sessionmaker(bind=engine)
+
+
+class Candidates(Base):
+    __tablename__ = 'Candidates'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(40))
+
+
+Base.metadata.create_all(engine)
